ccebb90882e7aad8eaf08f6cec342d6db7c195d0
AronShrestha <aronshrestha498@gmail.com>
2021-05-29 15:13:01 +0545

    backend with db for notes

---
+++ django/backend/notes/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+from django.core.validators import FileExtensionValidator
+
+
+# Create your models here.
+class Notes(models.Model):
+    date_posted = models.DateTimeField(default = timezone.now ) 
+    posted_By = models.CharField(max_length = 100)
+    description =  models.TextField()
+    title = models.CharField(max_length=100)
+    file =  models.FileField(upload_to='documents/',validators=[
+        FileExtensionValidator(allowed_extensions=['pdf', 'doc', 'ppt', 'xlsx'])])   
+
