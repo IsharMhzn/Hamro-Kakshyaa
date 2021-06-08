02334d24748bb78bc540f816d344632a5dce7a6a
SalinaK1 <saleena.koirala1@gmail.com>
2021-06-08 17:25:11 +0545

    JWT authentication added.
    Please visit Readme file for the instruction to install.

--- django/backend/backend/urls.py
+++ django/backend/backend/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from user.views import RegisterAPI
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('notes/',include('notes.urls')),
     path('notice/', include('notice.urls')),
-    path('', include('rest_framework.urls')),
-    path('', RegisterAPI.as_view())
+    # path('', include('rest_framework.urls')),
+    path('register', RegisterAPI.as_view())
 ]
