df7973e3e1245d7d92e98b61975b2a790822f38f
AronShrestha <aronshrestha498@gmail.com>
2021-06-08 15:15:09 +0545

    2mdel

--- django/backend/subject/models.py
+++ django/backend/subject/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class ClassCode(models.Model):
+    faculty = models.CharField(max_length=150)
+    batch = models.CharField(max_length=100)
+
+
+class SubjectCode(models.Model):
+    code = models.CharField(max_length=100)
+    #can we do it using dictionary for subject mapping sbject code 
+    
+
+
+
+
+
+
+
+
+ 
