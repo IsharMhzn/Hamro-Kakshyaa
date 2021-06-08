0bdbeccedb07835b931d855f4aa18abf731aff42
SalinaK1 <saleena.koirala1@gmail.com>
2021-06-08 10:41:25 +0545

    Uncommenting admine site.

--- django/backend/backend/urls.py
+++ django/backend/backend/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from user.views import RegisterAPI
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('notes/',include('notes.urls')),
     path('notice/', include('notice.urls')),
     path('', include('rest_framework.urls')),
